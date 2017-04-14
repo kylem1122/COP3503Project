674194fdf3dac0a431ccf4b2d5f5c15cd54cfe03
alanaer <ericksonalanal@gmail.com>
2017-04-14 17:49:38 -0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -26,37 +26,41 @@ int main(int argc, char ** argv){
 	int chanceArray [10][10];
     //load file into array
 	if(fileLoad == "load"){
-		ifstream in;
-			string file = "likelihood.txt";
-
-		 in.open(file);
-
-		if (!in.is_open()) {
-			cerr << "Missing file" << endl;
-			return 1;
-		}
-
-		int almostNum;
-		int a = 0;
-		int b = 0;
-		while (in >> almostNum){
-			chanceArray[a][b] = almostNum;
-			b++;
-			if(b == 10){
-				a++;
-				b = 0;
-			}
-			if(a == 10){
-				break;
-			}
-		}
-
-		for(int i = 0; i <10; i++){
-			for(int j = 0; j < 10; j++){
-				cout<< chanceArray[i][j] << " ";
-			}
-			cout << endl;
-		}  
+    ifstream in;
+    string file = "likelihood.txt";
+    
+    in.open(file.c_str());
+    
+    if (!in.is_open()) {
+        cerr << "Missing file" << endl;
+        return 1;
+    }
+    
+    int num;
+    int chanceArray [10][10];
+    int a = 0;
+    int b = 0;
+    
+    while (in >> num){
+        chanceArray[a][b] = num;
+        b++;
+        if(b == 10){
+            a++;
+            b = 0;
+        }
+        if(a == 10){
+            break;
+        }
+    }
+    
+    for(int i = 0; i <10; i++){
+        for(int j = 0; j < 10; j++){
+            cout<< chanceArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    in.close();
 	}
 	else{
 		for(int i = 0; i <10; i++){
