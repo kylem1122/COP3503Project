75cc024908f2bebd160637f4b0eae39581dae0c5
croot1 <chase.root@yahoo.com>
2017-04-14 16:58:03 -0400

    Update program.h

--- program.h
+++ program.h
@@ -7,9 +7,9 @@ using namespace std;
 
 //menu method
 int menu(){
+    cout<< "Welcome to War Boats"<< endl;
     int selection = 0;
     while(selection > 4 || selection < 0){
-    cout<< "Welcome to War Boats"<< endl;
     cout<< "Please select an option"<< endl;
     cout<< "1. Start Game "<< endl;
     cout<< "2. Place Pieces" << endl;
