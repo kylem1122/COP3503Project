53d993dbd5234b5d025eb0ddcd8d72a87df39728
croot1 <chase.root@yahoo.com>
2017-04-14 16:51:31 -0400

    Update program.h

--- program.h
+++ program.h
@@ -3,4 +3,21 @@
 
 //create classes
 
+//menu method
+int menu(){
+    int selection = 0;
+    while(selection > 4 || selection < 0){
+    cout<< "Welcome to War Boats"<< endl;
+    cout<< "Please select an option"<< endl;
+    cout<< "1. Start Game "<< endl;
+    cout<< "2. Place Pieces" << endl;
+    cout<< "3. Modify Pieces "<< endl;
+    cout<< "4. Exit "<< endl;
+    cin>> selection;
+    }
+    return selection;
+}
+
+//
+
 #endif
