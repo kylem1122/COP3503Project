91fb395d4e743b8ae0052687b58ecf84a979d3ed
sstingle <sstingle@ufl.edu>
2017-04-14 17:33:01 -0400

    Update program.h

--- program.h
+++ program.h
@@ -5,6 +5,69 @@
 #include <iostream>
 using namespace std; 
 
+class Item {
+protected:
+	bool hit = false;
+	int size = 0;
+	string name;
+public:
+	Item();
+	Item(bool hit, int size, string name);
+	bool isHit(bool hit) {
+	
+	}
+	string getName() {
+		return name;
+	}
+	int getSize() {
+		return size;
+	}
+	void setName(string newName) {
+		name = newName;
+	}
+	void setSize(int newSize) {
+		size = newSize;
+	}
+};
+
+class Map{
+	Item board[10][10];
+	string type;
+public:
+	Map();
+	void setType(string newtype) {
+		type = newtype;
+	}
+	string getType() {
+		return type;
+	}
+	void setShip(int xIndex, int yIndex, Item ship) {
+		board[xIndex][yIndex] = ship;
+	}
+
+	void setWater() {
+		for (int i = 0; i != 10; ++i) {
+			for (int j = 0; j != 10; ++j) {
+				board[j][i] = Item(false, 0, "W");
+			}
+		}
+	}
+	void print() {
+		Item temp;
+		string tempName;
+		for (int i = 0; i != 10; ++i) {
+			for (int j = 0; j != 10; ++j) {
+				temp = board[j][i];
+				tempName = temp.getName();
+				cout << tempName << endl;
+			}
+		}
+
+	}
+
+
+};
+
 //menu method
 int menu(){
     cout<< "Welcome to War Boats"<< endl;
