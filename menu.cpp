3828f923d56b6484193766b5d98aacca883688f5
croot1 <chase.root@yahoo.com>
2017-04-14 16:21:03 -0400

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int menu(){
+    int selection = 0;
+    while(selection > 4 || selection < 0){
+    cout<< "Welcome to War Boats"<< endl;
+    cout<< "Please select an option"<< endl;
+    cout<< "1. Start Game "<< endl;
+    cout<< "2. Place Pieces" << endl;
+    cout<< "3. Modify Pieces "<< endl;
+    cout<< "4. Exit "<< endl;
+    cin>> selection;
+    }
+    return selection:
+}
