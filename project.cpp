1c035c571dbe727a4eccf362ebd9b8870627f287
alanaer <ericksonalanal@gmail.com>
2017-04-14 17:13:36 -0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 
 int main(int argc, char ** argv){
-    string fileLoad;
-    transform(fileLoad.begin(), fileLoad.end(), fileLoad.begin(), ::tolower);
+  	string fileLoad;
+  	transform(fileLoad.begin(), fileLoad.end(), fileLoad.begin(), ::tolower);
 
 	if (argc >= 2) {
 	    fileLoad = argv[1];
@@ -20,7 +20,39 @@ int main(int argc, char ** argv){
 	}
 	
     //load file into array
-	
+	ifstream in;
+    	string file = "likelihood.txt";
+    
+   	 in.open(file);
+    
+    if (!in.is_open()) {
+        cerr << "Missing file" << endl;
+        return 1;
+    }
+    
+    int almostNum;
+    int chanceArray [10][10];
+    int a = 0;
+    int b = 0;
+    while (in >> almostNum){
+        chanceArray[a][b] = almostNum;
+        b++;
+        if(b == 10){
+            a++;
+            b = 0;
+        }
+        if(a == 10){
+            break;
+        }
+    }
+    
+    for(int i = 0; i <10; i++){
+        for(int j = 0; j < 10; j++){
+            cout<< chanceArray[i][j] << " ";
+        }
+        cout << endl;
+    }
+
 	
     //put stuff here!
     
