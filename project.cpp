104f1ee7057770a051a10ee05d5b6b6f619422cc
alanaer <ericksonalanal@gmail.com>
2017-04-15 01:05:56 -0400

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,10 +1,77 @@
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
 #include "project.h"
 
 using namespace std;
+
+bool previousHit = false;
+bool previousPreviousHit = false;
+int chanceArray [10][10];
+
+
+bool computerGuess(map& userMap) {
+    bool hitShip = false;
+    int max_x = 0;
+    int max_y = 0;
+    int max = 0;
+    
+    
+    //find first open index
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            if(!userMap.getItemAt(i,j).isHit()){
+                max_x = i;
+                max_y = j;
+                max = chanceArray[i][j];
+                break;
+            }
+        }
+    }
+    
+    
+    //find max index
+    for(int i = max_x; i < 10; i++){
+        for(int j = 0; j <10; j++){
+            if(chanceArray[i][j] > max){
+                if(rand()%2 && !userMap.getItemAt(i,j).isHit()){
+                    max_x = i;
+                    max_y = j;
+                    max = chanceArray[i][i];
+                }
+            }
+            else if(chanceArray[i][j] == max && !userMap.getItemAt(i,j).isHit()){
+                if(rand()%2){
+                    if(rand()%2){
+                        if(rand()%2){
+                            max_x = i;
+                            max_y = j;
+                            max = chanceArray[i][i];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    if(previousHit){
+        //do something
+    }
+    else{
+        userMap.getItemAt(max_x, max_y).itemHit();
+        if(userMap.getItemAt(max_x, max_y).isShip()){
+            hitShip = true;
+        }
+    }
+    
+    return hitShip;
+}
+
+bool nextGuess(int x, int y){
+    return true;
+}
 
 
 int main(int argc, char ** argv){
@@ -16,51 +83,51 @@ int main(int argc, char ** argv){
 	}
 	else {
 		cout << "Invalid choice. Try again." << endl;
-		return -1;
+		return 1;
 	}
 	if(fileLoad != "erase" || fileLoad != "load"){
 		cout << "Invalid choice. Try again." << endl;
-		return -1;
+		return 1;
 	}
 	
 	int chanceArray [10][10];
     //load file into array
 	if(fileLoad == "load"){
-    ifstream in;
-    string file = "likelihood.txt";
-    
-    in.open(file.c_str());
-    
-    if (!in.is_open()) {
-        cerr << "Missing file" << endl;
-        return 1;
-    }
-    
-    int num;
-    int chanceArray [10][10];
-    int a = 0;
-    int b = 0;
-    
-    while (in >> num){
-        chanceArray[a][b] = num;
-        b++;
-        if(b == 10){
-            a++;
-            b = 0;
+        ifstream in;
+        string file = "likelihood.txt";
+        
+        in.open(file.c_str());
+        
+        if (!in.is_open()) {
+            cerr << "Missing file" << endl;
+            return 1;
         }
-        if(a == 10){
-            break;
+        
+        int num;
+        int chanceArray [10][10];
+        int a = 0;
+        int b = 0;
+        
+        while (in >> num){
+            chanceArray[a][b] = num;
+            b++;
+            if(b == 10){
+                a++;
+                b = 0;
+            }
+            if(a == 10){
+                break;
+            }
         }
-    }
-    
-    for(int i = 0; i <10; i++){
-        for(int j = 0; j < 10; j++){
-            cout<< chanceArray[i][j] << " ";
+        
+        for(int i = 0; i <10; i++){
+            for(int j = 0; j < 10; j++){
+                cout<< chanceArray[i][j] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
-    }
-    
-    in.close();
+        
+        in.close();
 	}
 	else{
 		for(int i = 0; i <10; i++){
@@ -70,7 +137,8 @@ int main(int argc, char ** argv){
 		}
 	}
 
-	
+	map* userMap = new map();
+    computerGuess(*userMap);
     //put stuff here!
     
     
