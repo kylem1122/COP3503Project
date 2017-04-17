f3bb63b357a1a0625bdf18c4d67870e2a96cfb38
alanaer <ericksonalanal@gmail.com>
2017-04-17 01:21:44 -0400

    Update project.h

--- project.h
+++ project.h
@@ -5,71 +5,77 @@
 #include <iostream>
 using namespace std;
 
+//placeable item object
 class Item {
+    //variables for ship & hit identifying / name & size of item
 protected:
     bool hit = false;
     bool shipOrNoShip = true;
     int size = 0;
     string name;
 public:
-    Item();
-    Item(int size, string name, bool hit){
+    Item();             //default constructor
+    Item(int size, string name, bool hit){          //constructor to set size, name, hit
         this->size = size;
         this->name = name;
         this->hit = hit;
         if(name == "~"){
+            //if water, set the variable as not being a ship
             shipOrNoShip = false;
         }
     };
     
+    //checks if spot has been shot before -- getter method
     bool isHit() {
         return this->hit;
     }
     
-    void itemHit(){
-        this->hit = true;
-        if(this->isShip()){
-            this->name = "X";
-        }
-        else{
-            this->name = "O";
-        }
-    }
     
+    //returns item name -- getter method
     string getName() {
         return name;
     }
     
+    //returns item size -- getter method
     int getSize() {
         return size;
     }
     
+    //changes the name of item -- setter method
     void setName(string newName) {
         name = newName;
     }
     
+    //changes the size of the item -- setter method
     void setSize(int newSize) {
         size = newSize;
     }
     
+    //checks to see if the item is a placed ship -- getter method
     bool isShip(){
         return shipOrNoShip;
     }
 };
 
 
+//gameboard that game uses to place items on
 class Map{
+    //standard battleship board
     Item* board[10][10];
-    string type;
 public:
+    //default constructor
     Map(){
+        //automatically fills the baord with water
         setWater();
     };
     
+    //sets the ship with an item at a specific index
     void setShip(int xIndex, int yIndex, Item& ship) {
+        delete board[xIndex][yIndex];
         board[xIndex][yIndex] = &ship;
     }
     
+    //sets the entire board with water
     void setWater() {
         for (int i = 0; i != 10; ++i) {
             for (int j = 0; j != 10; ++j) {
@@ -79,14 +85,17 @@ public:
         }
     }
     
+    //returns the item at a specific index on the board
     Item getItemAt(int x, int y){
         return *board[x][y];
     }
     
+    //allows the user to set an item at an index
     void setItemAt(int x, int y, Item newItem){
         *board[x][y] = newItem;
     }
     
+    //checks if the board contains an item, returns true if it does
     bool containsItem(string name){
         for (int i = 0; i != 10; ++i) {
             for (int j = 0; j != 10; ++j) {
@@ -98,6 +107,7 @@ public:
         return false;
     }
     
+    //prints out the board
     void print() {
         
         const char separator    = ' ';
@@ -124,6 +134,7 @@ public:
         }
     }
     
+    //prints out the board with the ships hidden so the user doesn't know where they are located
     void printGameMap() {
         const char separator    = ' ';
         for (int i = 0; i != 10; ++i) {
